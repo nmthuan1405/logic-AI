74f271cd290d0b0b858cc6480245965ff2906cba
Nguyễn Minh Thuận <nmthuan.nmt@gmail.com>
2021-12-15 21:58:15 +0700

    [UPDATE] clean code

--- PS4/src/main.py
+++ PS4/src/main.py
@@ -5,7 +5,7 @@ import utils
 INPUT_FOLDER = "input/"
 OUTPUT_FOLDER = "output/"
 
-for file_name in os.listdir(INPUT_FOLDER):
-    alpha, KB = utils.read_input(os.path.join(INPUT_FOLDER, file_name))
+for filename in os.listdir(INPUT_FOLDER):
+    alpha, KB = utils.read_input(os.path.join(INPUT_FOLDER, filename))
     result = logic.pl_resolution(KB, alpha)
-    utils.write_output(os.path.join(OUTPUT_FOLDER, file_name), result)
+    utils.write_output(os.path.join(OUTPUT_FOLDER, utils.output_filename(filename)), result)
